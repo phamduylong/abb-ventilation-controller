07e6f8beb61a624b992d92505b0dc2885e690f4c
Duy Long Pham <longphamduy2002@gmail.com>
2022-09-22 14:02:56 +0300

    deleted assignment operator

--- main/src/DigitalIoPin.h
+++ main/src/DigitalIoPin.h
@@ -2,6 +2,7 @@ class DigitalIoPin {
 public:
  DigitalIoPin(int _port, int _pin, bool input = true, bool pullup = true, bool _invert = false);
  DigitalIoPin(const DigitalIoPin &) = delete; //no copy constructor allowed
+ DigitalIoPin& operator= (const DigitalIoPin&) = delete;
  virtual ~DigitalIoPin();
  bool read();
  void write(bool value);
