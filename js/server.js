dfe63892987be82fa3de1d701163b490fd9901cc
Duy Long Pham <longphamduy2002@gmail.com>
2022-10-17 00:28:55 +0300

    login page & new modules installed

--- js/server.js
+++ js/server.js
@@ -2,10 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const storage = require('node-sessionstorage');
 const path = require('path');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.set("views", (path.join(__dirname, "views")));
 
@@ -15,9 +18,19 @@ app.get('/', async (req, res) => {
     res.render('home');
 });
 
-app.post('/mode', async (req, res) => {
-    const mode = req.body;
-    console.log("BODY: ", mode);
+app.get('/auto', async (req, res) => {
+    res.render('auto');
+});
+
+app.get('/manual', async (req, res) => {
+    res.render('manual');
+});
+
+app.post('/login', async (req, res) => {
+    console.log(req.body);
+    const username = req.body.username;
+    const password = req.body.password;
+
     res.redirect('/');
 })
 
