6986ef78ce9dececbfd00850d8b663759b4e4cae
RedHawk <tadshig@gmail.com>
2022-10-19 16:40:18 +0300

    State Machine initializations.

--- main/src/StateMachine.cpp
+++ main/src/StateMachine.cpp
@@ -1,14 +1,63 @@
 #include "StateMachine.h"
 
-StateMachine::StateMachine(LpcUart *uart, LiquidCrystal *lcd) : uart(uart), lcd(lcd) {
+StateMachine::StateMachine(LpcUart *uart, LiquidCrystal *lcd, bool fast) : uart(uart), lcd(lcd), fast(fast) {
 	this->timeout = 5000;
+	this->rh = 0;
+	this->temp = 0;
+	this->co2 = 0;
+	this->pres = 0;
+
+	this->check_sensors();
+	
+	//Initialise all possible menu items.
+	this->mrhum = new DecimalShow(this->lcd, std::string("Rel Humidity"), std::string("%"));
+	this->mirhum = new MenuItem(this->mrhum);
+	this->mtemp = new DecimalShow(this->lcd, std::string("Temperature"), std::string("C"));
+	this->mitemp = new MenuItem(this->mtemp);
+	this->mco2 = new DecimalShow(this->lcd, std::string("CO2 Level"), std::string("%"));
+	this->mico2 = new MenuItem(this->mco2);
+	//Maybe it is better to add flag and title swapping?
+	this->mfana = new IntegerShow(this->lcd, std::string("Fan Speed"), std::string("%"));
+	this->mifana = new MenuItem(this->mfana);
+	this->mfanm = new IntegerEdit(this->lcd, std::string("Fan Speed     M"), 100, 0, 10, std::string("%"), true);
+	this->mifanm = new MenuItem(this->mfanm);
+	//Not quite what we need.
+	this->mpresa = new DecimalEdit(this->lcd, std::string("Pressure      M"), 100, 0, 10, std::string("%"), true);
+	this->mipresa = new MenuItem(this->mpresa);
+	this->mpresm = new DecimalShow(this->lcd, std::string("Pressure"), std::string("%"));
+	this->mipresm = new MenuItem(this->mpresm);
+
+	//Add all menu items to the menu, don't care for the mode just yet.
+	this->menu.addItem(this->mirhum);
+	this->menu.addItem(this->mitemp);
+	this->menu.addItem(this->mico2);
+	this->menu.addItem(this->mifana);
+	this->menu.addItem(this->mifanm);
+	this->menu.addItem(this->mipresa);
+	this->menu.addItem(this->mipresm);
+
+	//Go into init state
 	this->currentState = &StateMachine::sinit;
 	(this->*currentState)(Event(Event::eEnter));
-	this->mfanm = new IntegerUnitEdit(this->lcd, std::string("Fan Speed"),100,0,1,std::string("%"));
-	this->menu.addItem(new MenuItem(this->mfanm));
 }
 
-StateMachine::~StateMachine() {}
+//State machine should not be deleted at any point, but in case it happens - delete all allocated memory.
+StateMachine::~StateMachine() {
+	delete this->mrhum;
+	delete this->mirhum;
+	delete this->mtemp;
+	delete this->mitemp;
+	delete this->mco2;
+	delete this->mico2;
+	delete this->mfana;
+	delete this->mifana;
+	delete this->mfanm;
+	delete this->mifanm;
+	delete this->mpresa;
+	delete this->mipresa;
+	delete this->mpresm;
+	delete this->mipresm;
+}
 
 void StateMachine::SetState(state_ptr newState) {
 	(this->*currentState)(Event(Event::eExit));
@@ -39,22 +88,44 @@ void StateMachine::sinit(const Event& e) {
 		break;
 	case Event::eTick:
 		this->timer++;
-		if (this->timer >= this->timeout) SetState(&StateMachine::stemp);
+		if (this->timer >= this->timeout) SetState(&StateMachine::sauto);
 		break;
 	default:
 		break;
 	}
 }
 
-void StateMachine::stemp(const Event& e) {
+void StateMachine::sauto(const Event& e) {
 	switch (e.type) 
 	{
 	case Event::eEnter:
-		this->uart->write("Entered stemp.\r\n");
+		this->uart->write("Entered sauto.\r\n");
 		this->timer = 0;
 		break;
 	case Event::eExit:
-		this->uart->write("Exited stemp.\r\n");
+		this->uart->write("Exited sauto.\r\n");
+		break;
+	case Event::eSwitchAuto:
+
+		break;
+	case Event::eTick:
+		this->timer++;
+		if (this->timer >= this->timeout) SetState(&StateMachine::sinit);
+		break;
+	default:
+		break;
+	}
+}
+
+void StateMachine::smanual(const Event& e) {
+	switch (e.type) 
+	{
+	case Event::eEnter:
+		this->uart->write("Entered smanual.\r\n");
+		this->timer = 0;
+		break;
+	case Event::eExit:
+		this->uart->write("Exited smanual.\r\n");
 		break;
 	case Event::eSwitchAuto:
 
@@ -71,3 +142,36 @@ void StateMachine::stemp(const Event& e) {
 //////////////////////////////////////////////////////////////////////////////
 // Helpers ///////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
+
+void StateMachine::check_sensors() {
+	//We should be able to communicate with all sensors.
+	sfrht_up = this->srht.read_rhum(this->rh, !fast);
+	sfrht_up = this->srht.read_temp(this->temp, !fast);
+	sfco2_up = this->sco2.read(this->co2, !fast);
+	sfpres_up = this->spres.read(this->pres, !fast);
+}
+
+void StateMachine::readRelHum() {
+	this->sfrht_up = this->srht.read_rhum(this->rh, !fast);
+	this->operation_time += this->srht.get_elapsed_time();
+}
+
+void StateMachine::readTemp() {
+	this->sfrht_up = this->srht.read_temp(this->temp, !fast);
+	this->operation_time += this->srht.get_elapsed_time();
+}
+
+void StateMachine::readCo2() {
+	this->sfco2_up = this->sco2.read(this->co2, !fast);
+	this->operation_time += this->sco2.get_elapsed_time();
+}
+
+void StateMachine::readPres() {
+	this->sfpres_up = this->spres.read(this->pres, !fast);
+	this->operation_time += this->spres.get_elapsed_time();
+}
+
+void StateMachine::set_fan(int16_t speed) {
+	this->dffan_up = this->fan.set_speed(speed, !fast);
+	this->operation_time += this->fan.get_elapsed_time();
+}
--- main/src/StateMachine.h
+++ main/src/StateMachine.h
@@ -1,6 +1,7 @@
 #ifndef STATEMACHINE_H_
 #define STATEMACHINE_H_
 
+#include <stdio.h>
 #include "Event.h"
 #include "LpcUart.h"
 #include "DigitalIoPin.h"
@@ -10,7 +11,9 @@
 #include "dFanMIO12V.h"
 #include "SimpleMenu.h"
 #include "DecimalEdit.h"
+#include "DecimalShow.h"
 #include "IntegerEdit.h"
+#include "IntegerShow.h"
 #include "IntegerUnitEdit.h"
 #include "LiquidCrystal.h"
 
@@ -19,36 +22,69 @@ typedef void (StateMachine::*state_ptr)(const Event &);
 
 class StateMachine {
 public:
-	StateMachine(LpcUart *uart, LiquidCrystal *lcd);
+	StateMachine(LpcUart *uart, LiquidCrystal *lcd, bool fast = false);
 	virtual ~StateMachine();
 	void HandleState(const Event& e);
 private:
 	//States.
 	state_ptr currentState;
 	void sinit(const Event& e);
-	void stemp(const Event& e);
+	void sauto(const Event& e);
+	void smanual(const Event& e);
+
+	//Sensor errors.
+	enum sErr {sErno, sErrht, sErco2, sErpres, sErfan};
 	
 	//Functions.
 	void SetState(state_ptr newState);
+	void check_sensors();
+	void readRelHum();
+	void readTemp();
+	void readCo2();
+	void readPres();
+	void set_fan(int16_t speed);
 
-	//Variables.
+	//Uart, Lcd and timer.
 	LpcUart *uart; //uart for debug prints.
 	LiquidCrystal *lcd; //lcd display
 	int timer; //Counts ticks for state.
 	int timeout; //How many ticks will state remain.
+	//Menu.
 	SimpleMenu menu;
-	//DecimalEdit *mrhum;
-	//DecimalEdit *mtemp;
-	//DecimalEdit *mco2;
-	IntegerUnitEdit *mfanm;
-	//DecimalEdit *mpresm;
-	//IntegerUnitEdit *mfana;
-	//DecimalEdit *mpresa;
+	DecimalShow *mrhum;
+	DecimalShow *mtemp;
+	DecimalShow *mco2;
+	IntegerEdit *mfanm;
+	DecimalShow *mpresm;
+	IntegerShow *mfana;
+	DecimalEdit *mpresa;
+	MenuItem *mirhum;
+	MenuItem *mitemp;
+	MenuItem *mico2;
+	MenuItem *mifanm;
+	MenuItem *mipresm;
+	MenuItem *mifana;
+	MenuItem *mipresa;
 
+	//Sensors and devices.
 	sPressureSDP610 spres;
 	srhtHMP60 srht;
 	sco2GMP252 sco2;
 	dFanMIO12V fan;
+
+	//Variables.
+	float co2;
+	float temp;
+	float rh;
+	float pres;
+	//int16_t fan_speed;
+	unsigned int operation_time = 0; //Zeroed out only after all sensor readings.
+	bool fast; //Set true to skip retries.
+	//Sensor/Devices flags.
+	bool sfrht_up;
+	bool sfco2_up;
+	bool sfpres_up;
+	bool dffan_up;
 };
 
 #endif /* STATEMACHINE_H_ */
