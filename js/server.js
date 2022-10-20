cc70343bd32773578b140e647a112fdbc1c3fb81
Duy Long Pham <longphamduy2002@gmail.com>
2022-10-21 00:51:53 +0300

    user analytics curr iter, session duration bug

--- js/server.js
+++ js/server.js
@@ -215,3 +215,8 @@ const addUser = (newUser) => {
     });
 }
 
+const isSameDay = (d1, d2) => {
+    return d1.getFullYear() === d2.getFullYear() &&
+        d1.getMonth() === d2.getMonth() &&
+        d1.getDate() === d2.getDate();
+}
